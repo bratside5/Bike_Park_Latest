ee033d6e175e1efc3860a56d683a52e805486429
bratside5 <bratside5@gmail.com>
2021-05-13 12:38:24 +0200

    next config env

--- client-latest/next.config.js
+++ client-latest/next.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   env: {
     MAPBOX_TOKEN: process.env.MAPBOX_TOKEN,
+    API_URL: process.env.API_URL,
+    API_URL_PRODUCTION: process.env.API_URL_PRODUCTION,
+    API_URL_LOCAL: process.env.API_URL_LOCAL,
   },
   images: {
     domains: ["bikeparktignes.s3.eu-west-3.amazonaws.com"],
