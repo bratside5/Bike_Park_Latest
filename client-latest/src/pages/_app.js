715e18c45ba679c13a681e61ad2f8f6d74f14bb0
bratside5 <bratside5@gmail.com>
2021-05-21 07:49:46 +0200

    removed theme provider

--- client-latest/src/pages/_app.js
+++ client-latest/src/pages/_app.js
@@ -3,16 +3,13 @@ import "tailwindcss/tailwind.css";
 import "mapbox-gl/dist/mapbox-gl.css";
 
 import Layout from "@/components/layout";
-import { ThemeProvider } from "next-themes";
 import { appWithTranslation } from "next-i18next";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ThemeProvider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ThemeProvider>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
   );
 }
 
